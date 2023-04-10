71c279d49e8adb909828323a54ced5c5e3d51d28
Tadewos Somano <tadewos.ewalo@sparklingRocks.it>
2023-04-10 22:18:43 +0200

    minor modifications

--- .devcontainer/src/cpp_0001.cpp
+++ .devcontainer/src/cpp_0001.cpp
@@ -4,19 +4,24 @@
 #include <string_view>
 #include <compare>
 #include <set>
+#include <tuple>
+#include <typeinfo>
+
+#include<cxxabi.h>
 
 template<typename test>
 struct strct{
     using type = test;
-    explicit strct(type value):value_(value){}
-    void set_value(type value)
+    explicit strct(type value_1, type value_2):value_1_(value_1), value_2_(value_2) {}
+    void set_value(type value_1, type value_2)
     {
-        value_ = std::move(value); 
+        value_1_ = std::move(value_1);
+        value_2_ = std::move(value_2); 
     }
 
-    type get_value()
+    decltype(auto) get_value()
     {
-        return value_;
+        return std::tuple{value_1_, value_2_};
     }
     auto operator <=>(const strct& other) const = default;
 
@@ -25,38 +30,41 @@ struct strct{
     friend std::ostream& operator<<(std::ostream& os, const strct<T>& str);
 
     private:
-    type value_{};
+    type value_1_{};
+    type value_2_{};
 };
 
 template<typename test>
 std::ostream& operator<<(std::ostream& os, const strct<test>& str)
 {
-    os << "[ overloaded operator: " << str.value_ <<" ]";
-    os << "  size of value_ : [" << sizeof(str.value_) << "]\n";
+    int status = 0;
+    os << "[ overloaded operator value_1: " << str.value_1_ <<" ]";
+    os << "[ overloaded operator: value_2 " << str.value_2_ <<" ]\n";
+    os << "  size of : [" << abi::__cxa_demangle(typeid(test).name(),0 ,0,&status )<< "] " << sizeof(decltype(std::declval<test>())) << "\n";
     return os;
 
 }
 
 int main()
 {
-    strct/*<const char*>*/ tmplt_test_char("test const char*");                // const char* is deduced by default
+    strct/*<const char*>*/ tmplt_test_char("test const char* 1", "test const char* 2");                // const char* is deduced by default
     std::cout<< tmplt_test_char;
 
-    strct <std::string>  tmplt_test_string("test string");                     // specify template parameter to specify desired deduction other than default deduction
+    strct <std::string>  tmplt_test_string("test string 1", "test string 2");                          // specify template parameter to specify desired deduction other than default deduction
     std::cout<< tmplt_test_string;
 
-    strct <std::string_view>  tmplt_test_string_view("test string_view");      // specify template parameter to specify desired deduction other than default deduction
+    strct <std::string_view>  tmplt_test_string_view("test string_view 1", "test string_view 2");      // specify template parameter to specify desired deduction other than default deduction
     std::cout<< tmplt_test_string_view;
 
     // C++20 spaceship operator
-    strct test_1(0);
-    strct test_2(1);
+    strct test_1(0, 1);
+    strct test_2(1, 2);
     std::cout << std::boolalpha;
     std::cout << (test_1 <=test_2) << "\n";
     std::cout<< std::noboolalpha;
 
-    std::set<strct<int>> tests{strct(1), strct(2), strct(3), strct(4), strct(5), strct(6)};
+    std::set<strct<int>> tests{strct(0,1), strct(1,2), strct(2,3), strct(3,4), strct(4,5), strct(5,6)};
 
     for(auto value:tests) std::cout<< value<<"\n";
-
+    std::cout<< sizeof(int) << "\n";
 }
